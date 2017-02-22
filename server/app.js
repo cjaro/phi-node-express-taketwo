fd6be518ec06898aeea9ae58818c07f35db3c72e
Catherine J <cjarocki@gmail.com>
2017-02-22 11:34:22 -0600

    serving static files

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log('starting up the server, which is a lie right now');
+console.log('starting up the server');
+
+var express = require('express');
+var app = express();
+
+
+//this will run when we make a request
+app.use(express.static('server/public')); //is this request for a static file, like script.js, index.html, styles.css, etc
+
+app.listen(5000);
